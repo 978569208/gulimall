39aef10829216909dd531965595425a4d55df966
hukaili <as.hukaili@aisino.com>
2022-05-28 17:36:23 +0800

    修改nacos配置，增加网关跨域

--- gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
@@ -1,11 +1,15 @@
 package com.atguigu.gulimall.gateway;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
 @EnableDiscoveryClient
+@ComponentScan(value = "com.atguigu.gulimall")
 public class GulimallGatewayApplication {
 
     public static void main(String[] args) {
